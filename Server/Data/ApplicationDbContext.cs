6a3ca4c027f18ef3c1fd2d5ed9bda7029f9fcfa3
May <themaymeow@gmail.com>
2019-10-28 18:56:35 +0100

    Update dpContext

--- Server/Data/ApplicationDbContext.cs
+++ Server/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ using Server.Models;
 
 namespace Server.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> // Use the ApplicationUser type as a generic argument for the context
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
