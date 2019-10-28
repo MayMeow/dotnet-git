719cb163af20364d068b065b9789e062445eab87
May <themaymeow@gmail.com>
2019-10-28 16:27:51 +0100

    add projects

--- Server/Data/ApplicationDbContext.cs
+++ Server/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Server.Models;
 
 namespace Server.Data
 {
@@ -12,5 +13,7 @@ namespace Server.Data
             : base(options)
         {
         }
+
+        public DbSet<Project> Projects { get; set; }
     }
 }
