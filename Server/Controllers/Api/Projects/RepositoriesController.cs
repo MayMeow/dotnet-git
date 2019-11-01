818348534adc00a23b5c18ed04d710e01080c5ac
May <themaymeow@gmail.com>
2019-11-01 23:08:22 +0100

    Add api endpoint for Tree and Blob

--- Server/Controllers/Api/Projects/RepositoriesController.cs
+++ Server/Controllers/Api/Projects/RepositoriesController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Server.Config;
 using Server.Data;
+using Server.Models.Git;
 
 namespace Server.Controllers.Api.Projects
 {
@@ -29,47 +30,135 @@ namespace Server.Controllers.Api.Projects
             _config = config;
         }
 
+        /// <summary>
+        /// Returns items from repository
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("tree/{id}")]
-        public async Task<IEnumerable<TreeEntry>> GetTree(int id)
+        public async Task<List<TreeObject>> GetTree(int id)
         {
-            string sha = HttpContext.Request.Query["sha"];
-
+            string path = HttpContext.Request.Query["path"];
             var project = await _context.Projects.FindAsync(id);
+            _repository = new Repository(Path.Combine(_config.Value.Path, project.Name));
+            
+            var treeObjects = new List<Server.Models.Git.TreeObject>();
+            var tree = _repository.Head.Tip.Tree;
 
-            var repo = new Repository(_config.Value.Path + @"\" + project.Name);
-
-            //var tree = repo.Lookup<Tree>("48fe937f62cbb3a5cf6e1820348cf0a007aad529");
-            //var tree = repo.
-
-            if (sha == null)
+            if (string.IsNullOrEmpty(path))
             {
-                var tree = repo.Head.Tip.Tree;
-                return tree;
+                foreach (var item in tree)
+                {
+                    treeObjects.Add(new TreeObject { 
+                        Name = item.Name,
+                        Sha = item.Target.Sha,
+                        Type = item.TargetType.ToString(),
+                        Path = item.Path
+                    });
+                }
             } else
             {
-                var tree = repo.Lookup<Tree>(sha);
-                return tree;
+                var xxx = tree[path];
+
+                // For tree
+                if (xxx.TargetType == TreeEntryTargetType.Tree)
+                {
+                    foreach (var item in (Tree)xxx.Target)
+                    {
+                        treeObjects.Add(new TreeObject {
+                            Name = item.Name,
+                            Sha = item.Target.Sha,
+                            Type = item.TargetType.ToString(),
+                            Path = item.Path
+                        });
+                    }
+                }
+
+                // For blobs
+                if (xxx.TargetType == TreeEntryTargetType.Blob)
+                {
+                    var file = (Blob)xxx.Target;
+
+                    var blobObject = new TreeObject {
+                        Name = xxx.Name,
+                        Sha = xxx.Target.Sha,
+                        Type = xxx.TargetType.ToString(),
+                        Path = xxx.Path
+                    };
+
+                    if (!file.IsBinary)
+                    {
+                        string blobContent = null;
+                        using (var sr = new StreamReader(file.GetContentStream(), Encoding.UTF8))
+                        {
+                            blobContent = sr.ReadToEnd();
+                        }
+
+                        blobObject.Content = blobContent;
+                    }
+
+                    treeObjects.Add(blobObject);
+                }
             }
+
+            return treeObjects;
         }
 
+        /// <summary>
+        /// Return file content from repository if it is not binary
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("blob/{id}")]
-        public async Task<string> GetBlob(int id)
+        public async Task<TreeObject> GetBlob(int id)
         {
-            string content = null;
+            string path = HttpContext.Request.Query["path"];
+            var project = await _context.Projects.FindAsync(id);
+            _repository = new Repository(Path.Combine(_config.Value.Path, project.Name));
 
-            string sha = HttpContext.Request.Query["sha"];
+            var tree = _repository.Head.Tip.Tree;
+            var xxx = tree[path];
+
+            if (xxx != null)
+            {
+                if (xxx.TargetType == TreeEntryTargetType.Blob)
+                {
+                    var file = (Blob)xxx.Target;
+
+                    var blobObject = new TreeObject
+                    {
+                        Name = xxx.Name,
+                        Sha = xxx.Target.Sha,
+                        Type = xxx.TargetType.ToString(),
+                        Path = xxx.Path
+                    };
+
+                    if (!file.IsBinary)
+                    {
+                        string blobContent = null;
+                        using (var sr = new StreamReader(file.GetContentStream(), Encoding.UTF8))
+                        {
+                            blobContent = sr.ReadToEnd();
+                        }
+
+                        blobObject.Content = blobContent;
+                    }
+                    return blobObject;
+                }
+            }
+
+            return new TreeObject();
+        }
+
+        [HttpGet("history/{id}")]
+        public async Task<string> GetFileHistory(int id)
+        {
             var project = await _context.Projects.FindAsync(id);
             var repo = new Repository(_config.Value.Path + @"\" + project.Name);
 
+            var historyEntries = repo.Commits.QueryBy(".travis.yml").Select(h => h.Commit.Sha).Last();
 
-            var tree = repo.Lookup<Blob>(sha).GetContentStream();
-            //var meesage = repo.Commits;
-            using (var sr = new StreamReader(tree, Encoding.UTF8))
-            {
-                content = sr.ReadToEnd();
-            }
-            
-            return content;
+            return historyEntries;
         }
 
         [HttpGet("hello")]
